f916b8dc3d0619b572a2942375dfa6084f3c0e2b
Chris Roberts <chrisrobertspdx@gmail.com>
2017-09-01 19:45:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(board,index)# cod
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
+  elsif board[index] == "X" || board[index] == "O"
+    true
   end
 end #position_taken? method here!
